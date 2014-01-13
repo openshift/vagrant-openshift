2a96f09ce74f1f3995d70445b9b83e5da502934c
Krishna Raman <kraman@gmail.com>
2014-01-13 15:05:11 -0800

    Binary artifact suport

--- lib/vagrant-openshift/action/checkout_tests.rb
+++ lib/vagrant-openshift/action/checkout_tests.rb
@@ -28,6 +28,27 @@ module Vagrant
         def call(env)
           is_fedora = env[:machine].communicate.test("test -e /etc/fedora-release")
           sudo env[:machine], "cd #{Constants.build_dir}/builder; #{scl_wrapper(is_fedora,'rake checkout_tests')}"
+
+          sudo env[:machine], "mkdir -p /var/www/html/binaryartifacts"
+          sudo env[:machine], "restorecon /var/www/html/binaryartifacts"
+
+          remote_write(env[:machine], "/etc/httpd/conf.d/binaryartifacts.conf", "root:root", "0644") {
+            %{#
+# This configuration is to host the binary artifacts for use during testing.
+# Currently the apache only hosts on https and with a cert that is not signed
+# so curl requires -k.  To get around this in testing just host it over non-ssl
+# and test test copies the tgz file to the /var/www/html/binaryartifacts folder.
+#
+
+Listen 81
+
+<VirtualHost *:81>
+  ServerName localhost
+  ServerAdmin root@localhost
+  DocumentRoot /var/www/html/binaryartifacts
+</VirtualHost>}}
+
+          sudo env[:machine], "service httpd restart"
           @app.call(env)
         end
       end
