0b7fceb66ca807883c523d3df932de20d2210476
Jessica Forrester <jforrest@redhat.com>
2014-05-05 15:37:17 -0400

    Add sync-geard command

---
+++ lib/vagrant-openshift/command/repo_sync_geard.rb
@@ -0,0 +1,52 @@
+#--
+# Copyright 2013 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#++
+require_relative "../action"
+
+module Vagrant
+  module Openshift
+    module Commands
+      class RepoSyncGeard < Vagrant.plugin(2, :command)
+        include CommandHelper
+
+        def execute
+          options = {}
+
+          opts = OptionParser.new do |o|
+            o.banner = "Usage: vagrant sync-geard [vm-name]"
+            o.separator ""
+
+            o.on("-h", "--help", "Show this message") do |f|
+              options[:help] = f
+            end
+          end
+
+          # Parse the options
+          argv = parse_options(opts)
+          with_target_vms(argv, :reverse => true) do |machine|
+            if options[:help]
+              machine.env.ui.info opts
+              exit
+            end
+
+            actions = Vagrant::Openshift::Action.repo_sync_geard(options)
+            @env.action_runner.run actions, {:machine => machine}
+            0
+          end
+        end
+      end
+    end
+  end
+end
