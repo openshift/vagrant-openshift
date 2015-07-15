9f7c146dab2baec1360e8c9d51d45e06ad9c8e60
Michal Fojtik <mi@mifo.sk>
2015-07-15 18:08:44 +0200

    Fix constants

--- lib/vagrant-openshift/constants.rb
+++ lib/vagrant-openshift/constants.rb
@@ -40,8 +40,8 @@ module Vagrant
           'openshift/ruby-20'       => 'https://github.com/openshift/sti-ruby.git',
           'openshift/mysql-55'      => 'https://github.com/openshift/mysql.git',
           'openshift/mongodb-24'    => 'https://github.com/openshift/mongodb.git',
-          'openshift/postgresql-92' => 'https://github.com/openshift/postgresql.git'
-          'openshift/jenkins-16'    => 'https://github.com/openshift/jenkins.git'
+          'openshift/postgresql-92' => 'https://github.com/openshift/postgresql.git',
+          'openshift/jenkins-16'    => 'https://github.com/openshift/jenkins.git',
           'openshift/wildfly-8'     => 'https://github.com/openshift/wildfly-8-centos.git'
         }
       end
