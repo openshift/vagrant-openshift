ca3d77e0021d06c4f9a006b34e83cc861c2e5e94
Dan McPherson <dmcphers@redhat.com>
2015-09-12 20:16:40 -0400

    Reenable yum update

--- lib/vagrant-openshift/action.rb
+++ lib/vagrant-openshift/action.rb
@@ -38,7 +38,7 @@ module Vagrant
 
       def self.install_openshift(options)
         Vagrant::Action::Builder.new.tap do |b|
-          #b.use YumUpdate
+          b.use YumUpdate
           b.use SetHostName
           b.use InstallOpenshift
           b.use InstallOpenshiftRhel7
