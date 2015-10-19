e8b585b759a93d9e865d652d56aeacfb7d16560a
Dan McPherson <dmcphers@redhat.com>
2015-10-19 16:35:56 -0400

    Quiet ssh warnings

--- lib/vagrant-openshift/action/download_artifacts_openshift.rb
+++ lib/vagrant-openshift/action/download_artifacts_openshift.rb
@@ -54,7 +54,7 @@ module Vagrant
               FileUtils.mkdir_p File.dirname(target.to_s)
             end
 
-            command = "/usr/bin/rsync -az -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i #{private_key_path}' --rsync-path='sudo rsync' #{ssh_info[:username]}@#{ssh_info[:host]}:#{source} #{target}"
+            command = "/usr/bin/rsync -az -e 'ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i #{private_key_path}' --rsync-path='sudo rsync' #{ssh_info[:username]}@#{ssh_info[:host]}:#{source} #{target}"
 
             if not system(command)
               machine.ui.warn "Unable to download artifacts"
