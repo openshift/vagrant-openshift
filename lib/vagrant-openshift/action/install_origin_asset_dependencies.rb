4b015137759aed0ae1bee613a8e4ffbf8500bc05
Dan McPherson <dmcphers@redhat.com>
2015-11-06 22:45:12 -0500

    Revert "Skip asset installation if there is no diff on package.json or bower.json"
    This reverts commit ee136d32936b86a2552e0b22a1548730f5d3b1c5.

--- lib/vagrant-openshift/action/install_origin_asset_dependencies.rb
+++ lib/vagrant-openshift/action/install_origin_asset_dependencies.rb
@@ -37,51 +37,31 @@ if ! which npm > /dev/null 2>&1 ; then
   sudo yum -y install npm
 fi
 
-SKIP_INSTALL=1
 }
           if @options[:restore_assets]
             cmd += %{
+  # make sure the dirs always exist
+  mkdir -p /data/asset_dependencies/node_modules
+  mkdir -p /data/asset_dependencies/bower_components
   # copy them to the assets dir
-  if [[ -d $ASSET_BACKUP_DIR/node_modules ]]; then
-    cp -rf $ASSET_BACKUP_DIR/node_modules $ORIGIN_PATH/assets/node_modules
-  fi
-  if [[ -d $ASSET_BACKUP_DIR/bower_components ]]; then
-    cp -rf $ASSET_BACKUP_DIR/bower_components $ORIGIN_PATH/assets/bower_components
-  fi
-  if [[ -f $ASSET_BACKUP_DIR/go-bindata ]]; then
-    mkdir -p $ORIGIN_PATH/Godeps/_workspace/bin
-    cp -f $ASSET_BACKUP_DIR/go-bindata $ORIGIN_PATH/Godeps/_workspace/bin/go-bindata
-  fi
+  cp -rf $ASSET_BACKUP_DIR/node_modules $ORIGIN_PATH/assets/node_modules
+  cp -rf $ASSET_BACKUP_DIR/bower_components $ORIGIN_PATH/assets/bower_components
 
-  echo "Using restored assets, checking package.json and bower.json for updates..."
-  SKIP_INSTALL=0
-  diff $ORIGIN_PATH/assets/package.json $ASSET_BACKUP_DIR/package.json || SKIP_INSTALL=$?
-  if [[ $SKIP_INSTALL -eq 0 ]]; then
-    diff $ORIGIN_PATH/assets/bower.json $ASSET_BACKUP_DIR/bower.json || SKIP_INSTALL=$?
-  fi
 }
           end
 
           cmd += %{
-if [[ $SKIP_INSTALL -ne 0 ]]; then
-  pushd $ORIGIN_PATH
-    hack/install-assets.sh
-  popd
-fi
+pushd $ORIGIN_PATH
+  hack/install-assets.sh
+popd
 
 }
 
           if @options[:backup_assets]
             cmd += %{
-  if [[ SKIP_INSTALL -ne 0 ]]; then
-    rm -rf $ASSET_BACKUP_DIR
-    mkdir -p $ASSET_BACKUP_DIR
-    cp -r $ORIGIN_PATH/assets/node_modules $ASSET_BACKUP_DIR/node_modules
-    cp -r $ORIGIN_PATH/assets/bower_components $ASSET_BACKUP_DIR/bower_components
-    cp $ORIGIN_PATH/assets/package.json $ASSET_BACKUP_DIR/package.json
-    cp $ORIGIN_PATH/assets/bower.json $ASSET_BACKUP_DIR/bower.json  
-    cp $ORIGIN_PATH/Godeps/_workspace/bin/go-bindata $ASSET_BACKUP_DIR/go-bindata
-  fi
+  mkdir -p $ASSET_BACKUP_DIR
+  cp -rf $ORIGIN_PATH/assets/node_modules $ASSET_BACKUP_DIR/node_modules
+  cp -rf $ORIGIN_PATH/assets/bower_components $ASSET_BACKUP_DIR/bower_components
 }
           end
 
