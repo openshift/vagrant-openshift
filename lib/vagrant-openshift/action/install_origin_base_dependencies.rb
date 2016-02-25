29c163e0f03b56d5d565e031d819af58c32ec02f
Dan McPherson <dmcphers@redhat.com>
2016-02-24 19:25:27 -0500

    Check for lv instead of vg

--- lib/vagrant-openshift/action/install_origin_base_dependencies.rb
+++ lib/vagrant-openshift/action/install_origin_base_dependencies.rb
@@ -199,7 +199,7 @@ sed -i "s,^ADD_REGISTRY='\\(.*\\)',#ADD_REGISTRY='--add-registry=docker.io \\1',
 
 cat /etc/sysconfig/docker
 
-if sudo lvdisplay docker-vg 2>&1>/dev/null
+if sudo lvdisplay | grep docker-data 2>&1>/dev/null
 then
   sed -i "s,^DOCKER_STORAGE_OPTIONS=.*,DOCKER_STORAGE_OPTIONS='-s devicemapper --storage-opt dm.datadev=/dev/docker-vg/docker-data --storage-opt dm.metadatadev=/dev/docker-vg/docker-metadata'," /etc/sysconfig/docker-storage
 fi
