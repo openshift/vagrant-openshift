5f341a994e2164e62661f759d7785b756c06e3ef
Steve Kuznetsov <skuznets@redhat.com>
2016-06-08 14:41:32 -0400

    Use deferred deletion and removal for Docker daemon
    To combat:
     - https://bugzilla.redhat.com/show_bug.cgi?id=1344013
     - https://github.com/openshift/origin/issues/9201
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- lib/vagrant-openshift/action/install_origin_base_dependencies.rb
+++ lib/vagrant-openshift/action/install_origin_base_dependencies.rb
@@ -218,7 +218,7 @@ if [ -n "${VG}" ]
 then
   sudo lvcreate -n docker-data -l 90%FREE /dev/${VG}
   sudo lvcreate -n docker-metadata -l 50%FREE /dev/${VG}
-  sudo sed -i "s,^DOCKER_STORAGE_OPTIONS=.*,DOCKER_STORAGE_OPTIONS='-s devicemapper --storage-opt dm.datadev=/dev/${VG}/docker-data --storage-opt dm.metadatadev=/dev/${VG}/docker-metadata'," /etc/sysconfig/docker-storage
+  sudo sed -i "s,^DOCKER_STORAGE_OPTIONS=.*,DOCKER_STORAGE_OPTIONS='-s devicemapper --storage-opt dm.datadev=/dev/${VG}/docker-data --storage-opt dm.metadatadev=/dev/${VG}/docker-metadata --storage-opt dm.use_deferred_removal=true --storage-opt dm.use_deferred_deletion=true'," /etc/sysconfig/docker-storage
 
   sudo lvcreate -n openshift-xfs-vol-dir -l 100%FREE /dev/${VG}
   sudo mkfs.xfs /dev/${VG}/openshift-xfs-vol-dir
