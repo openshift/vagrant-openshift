8b1ec3e648ff8e7640ae68deb5f8c35799d60938
Steve Kuznetsov <skuznets@redhat.com>
2016-11-11 12:19:08 -0500

    Revert "Revert "Allow for users to specify Golang and Docker versions and RPM repos""

---
+++ lib/vagrant-openshift/action/install_docker.rb
@@ -0,0 +1,54 @@
+#--
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#++
+
+module Vagrant
+  module Openshift
+    module Action
+      class InstallDocker
+        include CommandHelper
+        include InstallHelper
+
+        def initialize(app, env, options)
+          @app = app
+          @env = env
+          @options = options.clone
+        end
+
+        def call(env)
+          # Migrate our install script to the host machine
+          ssh_user = @env[:machine].ssh_info[:username]
+          destination="/home/#{ssh_user}/"
+          @env[:machine].communicate.upload(File.join(__dir__,"/../resources/configure_docker.sh"), destination)
+          home="#{destination}/resources"
+
+          isolated_install(
+            @env[:machine],
+            'docker',
+            @options[:"docker.version"],
+            @options[:"docker.repourls"],
+            @options[:"docker.reponames"],
+            @options[:force]
+          )
+
+          # Configure the Docker daemon
+          sudo(@env[:machine], "SSH_USER='#{ssh_user}' #{home}/configure_docker.sh", :timeout=>60*30)
+
+          @app.call(@env)
+        end
+      end
+    end
+  end
+end
